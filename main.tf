633a09b8aee902068e33912302e420bf61fba71e
Vikas Rawat <vikasrawatengg@gmail.com>
2026-05-21 08:31:44 +0530

    added study files

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
 
-resource "aws_s3_bucket" "abc" {
-    bucket = "vikas12341"
+
+# resource "aws_s3_bucket" "abc" {
+#     bucket = var.bucket_name
+# }
+
+# resource "aws_s3_bucket" "abc1" {
+#     bucket = "jatin1234112121"
+# }
+
+
+
+resource "aws_ec2_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 }
